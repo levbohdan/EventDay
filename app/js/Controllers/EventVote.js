2b8c0c7d3d3f83e9d8a22709e18eea51f3a0e3f5
levbohdan <nookvel@gmail.com>
2014-02-05 18:17:35 +0200

    First Late Commit

---
+++ app/js/Controllers/EventVote.js
@@ -0,0 +1,28 @@
+app.controller("EventVoteCtrl", ["$scope", function (scopeCtrl) {
+    scopeCtrl.events = [
+        {
+            name: 'Birthday',
+            availableDays: [
+                {date: new Date(2014,1,7), vote: 3},
+                {date: new Date(2014,1,8), vote: 2},
+                {date: new Date(2014,1,9), vote: 1}
+            ]
+        },
+        {
+            name: 'Picnic',
+            availableDays: [
+                {date: new Date(2014,1,3), vote: 0},
+                {date: new Date(2014,1,5), vote: 0}
+            ]
+        },
+        {
+            name: 'Party',
+            availableDays: [
+                {date: new Date(2014,1,7), vote: 0},
+                {date: new Date(2014,1,14), vote: 0}
+            ]
+        }
+    ];
+    scopeCtrl.reverse=false;
+    scopeCtrl.byOrder="vote";
+}]);
